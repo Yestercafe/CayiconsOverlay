6d7af12ff10a02f52346ee8cf2eca8bf91467e95
Ivan Chien <qyc027@gmail.com>
2022-11-30 17:38:10 +0800

    Initial commit

---
+++ sync-with-cayicons.sh
@@ -0,0 +1,8 @@
+cd ./cayicons-ori && git pull
+rm -rf ./app/src/main/res
+mkdir -p ./app/src/main/res
+cp -r ./cayicons-ori/app/src/main/res/drawable ./app/src/main/res/
+cp -r ./cayicons-ori/app/src/main/res/xml ./app/src/main/res/
+mkdir -p ./app/src/main/res/values
+cp ./cayicons-ori/app/src/main/res/values/arrays.xml ./app/src/main/res/values/
+cp ./cayicons-ori/app/src/main/res/values/strings.xml ./app/src/main/res/values/
