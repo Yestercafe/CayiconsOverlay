d8299ff9999ea4152dd093937e5947a1ba870d0e
Ivan Chien <qyc027@gmail.com>
2022-11-30 17:41:33 +0800

    Update sync-with-cayicons

--- sync-with-cayicons.sh
+++ sync-with-cayicons.sh
@@ -1,8 +1,9 @@
 cd ./cayicons-ori && git pull
+cd ..
 rm -rf ./app/src/main/res
 mkdir -p ./app/src/main/res
 cp -r ./cayicons-ori/app/src/main/res/drawable ./app/src/main/res/
 cp -r ./cayicons-ori/app/src/main/res/xml ./app/src/main/res/
 mkdir -p ./app/src/main/res/values
 cp ./cayicons-ori/app/src/main/res/values/arrays.xml ./app/src/main/res/values/
-cp ./cayicons-ori/app/src/main/res/values/strings.xml ./app/src/main/res/values/
+cp ./cayicons-ori/app/src/main/res/values/dimens.xml ./app/src/main/res/values/
